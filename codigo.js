b621533a587c18d4c83d182f2aec6c392e1d9034
Samuel-Sierra <bocalin@hotmail.com>
2023-12-05 21:41:58 -0600

    funcionalidad de cola

--- codigo.js
+++ codigo.js
@@ -1,1 +1,20 @@
 //aaaaaaaaaaaaaa
+//codigo para cola
+function newQueue(){
+    const datosCola = document.getElementById("datosCola");
+    const inputT = document.createElement("input");
+    inputT.setAttribute("id", "cajaTC");
+    datosCola.appendChild(inputT);
+}
+function enqueueQueue(){
+    let valor = document.getElementById("cajaTC");
+    const cola = document.getElementById("cola");
+    const nuevoNodo = document.createElement("input");
+    nuevoNodo.value = valor.value;
+    nuevoNodo.readOnly=true;
+    nuevoNodo.setAttribute("class","colaC");
+    cola.insertBefore(nuevoNodo, cola.firstChild);
+}
+function dequeueQueue(){
+    
+}
