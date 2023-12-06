56b9f5a86ce9f0c829fe3ab56cdd279409271a18
Samuel-Sierra <bocalin@hotmail.com>
2023-12-06 14:01:22 -0600

    mas cambios en cola

--- codigo.js
+++ codigo.js
@@ -11,6 +11,7 @@ function newQueue(){
     }
     
 }
+
 function enqueueQueue(){
     let valor = document.getElementById("cajaTC");
     const cola = document.getElementById("cola");
@@ -20,20 +21,47 @@ function enqueueQueue(){
     nuevoNodo.setAttribute("class","colaC");
     cola.insertBefore(nuevoNodo, cola.firstChild);
 }
+
 function dequeueQueue(){
     const cola = document.getElementById("cola");
     cola.removeChild(cola.lastChild);
 }
+
+function deleteCookies(){
+    document.cookie.split(";").forEach(function(c) {
+        document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+      });
+}
+
 function storeQueue(){
+    deleteCookies();
     const cola = document.getElementById("cola");
     const longitudCola = document.querySelectorAll('#cola .colaC').length;
-    alert(longitudCola);
     for(let i=0; i<longitudCola; i++){
         let elemento = cola.getElementsByTagName("input")[i].value;
-        document.cookie = "elemento"+"i="+elemento+";max-age=120";
+        document.cookie = "elemento"+"i="+ encodeURIComponent(elemento)+";max-age=120";
     }
     
 }
+
+function showCookies(name){
+    var nameEQ = name + "="; 
+    var ca = document.cookie.split(';');
+    let cookiesC;
+
+    for(var i=0;i < ca.length;i++) {
+
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) {
+      cookiesC = decodeURIComponent( c.substring(nameEQ.length,c.length) );
+    }
+
+  }
+    
+    alert(cookiesC);
+}   
+
 function downloadQueue(){
     const cookies = document.cookie;
     const colaGuardada = cookies.lastIndexOf();
