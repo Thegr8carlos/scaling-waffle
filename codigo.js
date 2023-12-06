59d991e7f52bbcf642470e425e27b88a767c099d
Samuel-Sierra <bocalin@hotmail.com>
2023-12-06 08:26:48 -0600

    cambios en cola

--- codigo.js
+++ codigo.js
@@ -1,10 +1,15 @@
 //aaaaaaaaaaaaaa
 //codigo para cola
+let contC=0;
 function newQueue(){
-    const datosCola = document.getElementById("datosCola");
-    const inputT = document.createElement("input");
-    inputT.setAttribute("id", "cajaTC");
-    datosCola.appendChild(inputT);
+    if(contC===0){
+        const datosCola = document.getElementById("datosCola");
+        const inputT = document.createElement("input");
+        inputT.setAttribute("id", "cajaTC");
+        datosCola.appendChild(inputT);
+        contC=contC+1;
+    }
+    
 }
 function enqueueQueue(){
     let valor = document.getElementById("cajaTC");
@@ -16,5 +21,23 @@ function enqueueQueue(){
     cola.insertBefore(nuevoNodo, cola.firstChild);
 }
 function dequeueQueue(){
+    const cola = document.getElementById("cola");
+    cola.removeChild(cola.lastChild);
+}
+function storeQueue(){
+    const cola = document.getElementById("cola");
+    const longitudCola = document.querySelectorAll('#cola .colaC').length;
+    alert(longitudCola);
+    for(let i=0; i<longitudCola; i++){
+        let elemento = cola.getElementsByTagName("input")[i].value;
+        document.cookie = "elemento"+"i="+elemento+";max-age=120";
+    }
     
+}
+function downloadQueue(){
+    const cookies = document.cookie;
+    const colaGuardada = cookies.lastIndexOf();
+    alert(colaGuardada.toString);
+    const cola = document.getElementById("cola");
+    cola.appendChild(colaGuardada);
 }
